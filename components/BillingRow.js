c0d8a01198478dc5ecd35eb9ff445cdc3154bcf5
TheThomasY <twy1g16@soton.ac.uk>
2022-03-04 12:03:34 +0000

    feat: toggle fully styled, no state yet

--- components/BillingRow.js
+++ components/BillingRow.js
@@ -7,7 +7,10 @@ export default function BillingRow() {
   return (
     <StyledBillingRow>
       <p>Monthly Billing</p>
-      <div>Tog</div>
+      <CheckBoxWrapper>
+        <CheckBox id='checkbox' type='checkbox' />
+        <CheckBoxLabel htmlFor='checkbox' />
+      </CheckBoxWrapper>
       <p>Yearly Billing</p>
       <p>25%</p>
     </StyledBillingRow>
@@ -21,5 +24,51 @@ const StyledBillingRow = styled.div`
   border-bottom: 2px solid ${({ theme }) => theme.colors.sliderEmpty};
   display: flex;
   justify-content: flex-end;
+  align-items: center;
   gap: 1rem;
+`;
+
+const CheckBoxWrapper = styled.div`
+  position: relative;
+`;
+
+const CheckBoxLabel = styled.label`
+  position: absolute;
+  top: 0.3rem;
+  left: 0;
+  width: 4.4rem;
+  height: 2.2rem;
+  border-radius: 15px;
+  background: ${({ theme }) => theme.colors.toggleBg};
+  cursor: pointer;
+  &::after {
+    content: '';
+    display: block;
+    border-radius: 50%;
+    width: 1.4rem;
+    height: 1.4rem;
+    margin: 4px;
+    background: #ffffff;
+    transition: 0.2s;
+  }
+`;
+
+const CheckBox = styled.input`
+  opacity: 0;
+  z-index: 1;
+  border-radius: 15px;
+  width: 4.4rem;
+  height: 2.2rem;
+  &:checked + ${CheckBoxLabel} {
+    background: ${({ theme }) => theme.colors.sliderBar};
+    &::after {
+      content: '';
+      display: block;
+      border-radius: 50%;
+      width: 1.4rem;
+      height: 1.4rem;
+      margin-left: 2.7rem;
+      transition: 0.2s;
+    }
+  }
 `;
